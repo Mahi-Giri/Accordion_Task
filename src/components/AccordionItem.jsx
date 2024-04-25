be04816aae03a78e7b855a344fb3f63c700d9d72
Mahi <girimahesh.m@gmail.com>
2024-04-25 15:09:13 +0530

    Optimized Code:)

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -37,23 +37,23 @@ const AccordionItem = ({ open, toggle, title, desc, editable, index }) => {
     function isEmptyObject(obj) {
         return Object.keys(obj).length === 0;
     }
-
+    
     const handleCancel = () => {
         setShowBtn(false);
         const savedData = JSON.parse(localStorage.getItem(index)) || {};
-
+        let defaultFormData = {};
+    
         if (isEmptyObject(savedData)) {
-            const defaultFormData = {};
             desc.forEach((data, index) => {
                 defaultFormData[index] = "No";
             });
-
-            setFormData(defaultFormData);
         } else {
-            const defaultFormData = Object.assign({}, savedData);
-            setFormData(defaultFormData);
+            defaultFormData = {...savedData};
         }
+    
+        setFormData(defaultFormData);
     };
+    
 
     return (
         <div className="pt-2 text-black">
